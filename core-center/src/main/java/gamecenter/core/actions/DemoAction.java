2c7c3ed93125b65f8df4758d271b7cf11e201eb5
chevismart <chevismart@gmail.com>
2014-12-08 00:13:24 +0800

    Added Struts2 & Spring dependency.

---
+++ core-center/src/main/java/gamecenter/core/actions/DemoAction.java
@@ -0,0 +1,10 @@
+package gamecenter.core.actions;
+
+/**
+ * Created by Chevis on 2014/12/7.
+ */
+public class DemoAction {
+    public String hello(){
+        return "success";
+    }
+}
---
+++ core-center/src/main/java/gamecenter/core/beans/DemoBean.java
@@ -0,0 +1,7 @@
+package gamecenter.core.beans;
+
+/**
+ * Created by Chevis on 2014/12/8.
+ */
+public class DemoBean {
+}

35ba3b31cce268bc909822cad5de860685f3d5f1
chevismart <chevismart@gmail.com>
2014-12-15 02:26:09 +0800

    Add watch dog for the wechat access token update.

--- core-center/src/main/java/gamecenter/core/processors/tasks/wechatTokenManager.java
+++ core-center/src/main/java/gamecenter/core/processors/tasks/wechatTokenManager.java
@@ -1,13 +1,17 @@
 package gamecenter.core.processors.tasks;
 
+import gamecenter.core.Constants.CommonConstants;
 import gamecenter.core.listeners.AbstractRunnable;
 import gamecenter.core.processors.wechat.ProfileManager;
-import org.springframework.scheduling.TaskScheduler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Created by Chevis on 2014/12/11.
  */
 public class wechatTokenManager extends AbstractRunnable {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     private ProfileManager profileManager;
 
@@ -26,6 +30,19 @@ public class wechatTokenManager extends AbstractRunnable {
 
     @Override
     public void run() {
+
+        while (isContinue) {
+            try {
+                //休眠时间
+                Thread.sleep(CommonConstants.DEFAULT_WECHAT_ACCESS_TOKEN_CHECK_INTERVAL_IN_SECOND);
+                logger.debug("Start to check the access token.");
+                profileManager.checkAndUpdateAllAccessToken();
+
+            } catch (InterruptedException e) {
+                logger.error(e.getMessage());
+            }
+
+        }
 
     }
 }

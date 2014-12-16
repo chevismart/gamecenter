e282cb12b170381663e446d8ec1a6807eac66364
chevismart <chevismart@gmail.com>
2014-12-17 00:33:30 +0800

    UPDATE FILE

--- core-center/src/main/java/gamecenter/core/Constants/ComConstants.java
+++ core-center/src/main/java/gamecenter/core/Constants/ComConstants.java
@@ -3,7 +3,7 @@ package gamecenter.core.Constants;
 /**
  * Created by Chevis on 2014/12/11.
  */
-public class CommonConstants {
+public class ComConstants {
     public static final String WECHAT_AUTH_STATE ="STATE";
     public static final String TASKS_BEAN_NAME = "tasks";
     public static final String TASK_EXECUTOR_NAME = "taskExecutor";

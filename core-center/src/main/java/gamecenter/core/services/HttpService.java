f307565922fc996978b9a46394cc38c91abfa266
Chevis <chevismart@gmail.com>
2016-01-11 00:01:55 +0800

    Add timeout for http service as well as testcase

--- core-center/src/main/java/gamecenter/core/services/HttpService.java
+++ core-center/src/main/java/gamecenter/core/services/HttpService.java
@@ -4,6 +4,7 @@ import com.opensymphony.xwork2.util.logging.Logger;
 import com.opensymphony.xwork2.util.logging.LoggerFactory;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
@@ -15,24 +16,35 @@ import java.io.IOException;
 import java.util.List;
 
 import static java.util.Arrays.asList;
+import static org.apache.http.client.utils.URLEncodedUtils.format;
 
 public class HttpService {
+    public static final int HTTP_TIMEOUT = 2000;
     private static Logger logger = LoggerFactory.getLogger(HttpService.class);
     public static HttpClient httpClient = HttpClientFactory.createHttpClient();
 
     public static HttpResponse get(String uri, BasicNameValuePair... params) throws IOException {
         HttpGet httpGet = new HttpGet(uri.concat("?").concat(getParams(params)));
+        httpGet.setConfig(getHttpConfig());
         return httpClient.execute(httpGet);
     }
 
     public static HttpResponse post(String uri, List<BasicNameValuePair> params) throws IOException {
         HttpPost httpPost = new HttpPost(uri);
+        httpPost.setConfig(getHttpConfig());
         httpPost.setEntity(new UrlEncodedFormEntity(params));
         return httpClient.execute(httpPost);
     }
 
+    private static RequestConfig getHttpConfig() {
+        return RequestConfig.custom()
+                .setSocketTimeout(HTTP_TIMEOUT)
+                .setConnectTimeout(HTTP_TIMEOUT)
+                .build();
+    }
+
     private static String getParams(BasicNameValuePair... params) {
-        return URLEncodedUtils.format(asList(params), "UTF-8");
+        return format(asList(params), "UTF-8");
     }
 
 }
---
+++ core-center/src/test/java/gamecenter/core/services/HttpServiceTest.java
@@ -0,0 +1,97 @@
+package gamecenter.core.services;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.conn.HttpHostConnectException;
+import org.apache.http.message.BasicNameValuePair;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.SocketTimeoutException;
+import java.util.Collections;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class HttpServiceTest {
+
+    public static final String URI = "http://localhost:8888/test";
+    private HttpServer httpServer;
+    private String content;
+    private boolean isWaiting;
+
+    @Before
+    public void setUp() throws Exception {
+        httpServer = HttpServer.create(new InetSocketAddress(8888), 0);
+        httpServer.createContext("/test", new HttpHandler() {
+            @Override
+            public void handle(HttpExchange httpExchange) throws IOException {
+                if (isWaiting) try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                content = RandomStringUtils.randomAlphabetic(10);
+                httpExchange.sendResponseHeaders(HttpStatus.SC_OK, content.length());
+                OutputStream os = httpExchange.getResponseBody();
+                os.write(content.getBytes());
+                os.close();
+            }
+        });
+        httpServer.setExecutor(null);
+        httpServer.start();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        httpServer.stop(1);
+        isWaiting = false;
+    }
+
+    @Test
+    public void canSendPostMethodMessageAndGetResponse() throws Exception {
+        assertThat(getContent(HttpService.get(URI)), is(this.content));
+    }
+
+    @Test
+    public void canSendGetMethodMessageAndGetResponse() throws Exception {
+        assertThat(getContent(HttpService.post(URI, Collections.<BasicNameValuePair>emptyList())), is(this.content));
+    }
+
+    @Test(expected = SocketTimeoutException.class)
+    public void exceptionWillBeThrownIfTimeoutForGetMethod() throws Exception {
+        isWaiting = true;
+        HttpService.get(URI);
+    }
+
+    @Test(expected = SocketTimeoutException.class)
+    public void exceptionWillBeThrownIfTimeoutForPostMethod() throws Exception {
+        isWaiting = true;
+        HttpService.post(URI, Collections.<BasicNameValuePair>emptyList());
+    }
+
+    @Test(expected = HttpHostConnectException.class)
+    public void exceptionWillBeThrownIfNotAvailableForConnectForGet() throws Exception {
+        httpServer.stop(1);
+        HttpService.get(URI);
+    }
+
+    @Test(expected = HttpHostConnectException.class)
+    public void exceptionWillBeThrownIfNotAvailableForConnectForPost() throws Exception {
+        httpServer.stop(1);
+        HttpService.post(URI, Collections.<BasicNameValuePair>emptyList());
+    }
+
+    private String getContent(HttpResponse httpResponse) throws IOException {
+        return IOUtils.toString(httpResponse.getEntity().getContent());
+    }
+}
